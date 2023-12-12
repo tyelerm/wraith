acf7b5ab62c8d73604fa9ee88381516c5cf6f999
JeffreyGHJ <JeffreyGHJ@gmail.com>
2023-12-12 00:44:31 -0500

    edit scrollbar style, margins/padding

--- app/dashboard/leaderboard/page.tsx
+++ app/dashboard/leaderboard/page.tsx
@@ -6,7 +6,7 @@ import CreateUser from "./components/create/CreateUser";
 
 export default function Users() {
     return (
-        <div className="w-full max-w-[1400px] m-auto px-3 space-y-5 overflow-y-auto">
+        <div className="w-full max-w-[1400px] ml-auto mr-auto pt-4 px-3 space-y-5 overflow-y-auto">
             <h1 className="text-3xl font-bold">Leaderboard</h1>
             <div className="flex gap-2">
                 <SearchUser />
